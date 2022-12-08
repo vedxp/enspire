0427dbb161cac2d4f53cfdb9ced09b9424c3219f
Ved Prakash <87353286+vedxp@users.noreply.github.com>
2022-12-08 18:07:06 +0530

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,10 +10,11 @@ export default defineConfig({
   themeConfig: {
   footer: {
       message: "Released under the GPL-3.0 License.",
-      copyright: "Copyright © 2022-present VedxP" },
+      copyright: "Copyright 2022-present Vedxp" },
 
   nav: [
-    { text: 'GitHub', link: 'https://github.com/VedxP/enspire' }
+    { text: 'Source Code', link: 'https://github.com/vedxp/enspire' },
+    { text: 'Maintainer', link: 'https://vedxp.vercel.app/' }
     ],
   }
 })
